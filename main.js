3f22fdf31a64a4568c135f82ab2c8014c2806ec4
Noah Swerhun <nswerhun@gmail.com>
2018-03-25 14:31:54 -0500

    commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//string var- surronded in single quotes
+//number var- not surrounded in any quotes
+//variable rules
+//letters, numbers, underscores, and dollar signs.
+//begin with a letter
+//case sensitive
+var number1 = 35;
+var number2 = 40;
+
+alert(number1+number2);
